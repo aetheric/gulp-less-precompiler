6837b3b808f3d653ef869f1076fee14b772f5e16
pcummuskey <pcummuskey@bravurasolutions.com>
2015-10-31 17:25:44 +0000

    Added actual tests.

---
+++ src/test/plugin.spec.js
@@ -0,0 +1,43 @@
+/* global require, describe, it, Promise */
+'use strict';
+
+var assert = require('assert');
+var vinyl = require('vinyl-fs');
+var files = require('fs');
+var gutil = require('gulp-util');
+
+var precompiler = require('../..');
+
+describe('The gulp-less-precompiler', function() {
+
+	describe('when reading vinyl files', function() {
+
+		var expected = new Promise(function(resolve, reject) {
+			files.readFile('src/test/files/expected.less', function(error, result) {
+				return error ? reject(error) : resolve(result);
+			});
+		});
+
+		it('should replace imports in less files', function(done) {
+
+			vinyl.src('src/test/files/one.less')
+
+				.pipe(precompiler())
+
+				.once('data', function(file) {
+					gutil.log('File received: ' + file.path);
+					expected.then(function(result) {
+						assert.equal(file.contents.toString('utf8'), result.toString());
+						done();
+					}).catch(function(error) {
+						done(error);
+					})
+				})
+
+				.on('error', gutil.log);
+
+		});
+
+	});
+
+});
