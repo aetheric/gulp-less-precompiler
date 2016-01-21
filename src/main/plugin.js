4c4aaadf66550345cdfefd5117494887319a567f
Peter Cummuskey <pcummuskey@bravurasolutions.com>
2016-01-21 16:40:03 +1300

    [gh-9] Split the plugin and scanner code up. Added a debug counter.

--- src/main/plugin.js
+++ src/main/plugin.js
@@ -3,92 +3,9 @@
 
 var map = require('vinyl-map2');
 var gutil = require('gulp-util');
-var readline = require('readline');
-var files = require('fs');
-var paths = require('path');
-var StringBuilder = require('stringbuilder');
 var Promise = require('promise');
 
-/**
- *
- * @param {File} filepath The path to the file being scanned.
- * @param {Promise<Buffer>} promise A promise resolving to the buffer output.
- * @param {Object} context
- * @param {Boolean} debug Whether to display debug messages during work.
- * @returns {Promise<Buffer>}
- */
-function scanFile(filepath, promise, context, debug) {
-	return new Promise(function(resolve, reject) {
-
-		// Make sure the file hasn't already been processed
-		if (context[filepath]) {
-			gutil.log('===> ' + filepath + ' already processed.');
-			return promise.then(resolve, reject);
-		}
-
-		gutil.log('===> Processing ' + filepath);
-
-		// Add target file to the context to avoid repeats.
-		context[filepath] = true;
-
-		// Start the promise chain.
-		var lastPromise = promise;
-
-		var lineStream = readline.createInterface({
-			input: files.createReadStream(filepath),
-			terminal: false
-		});
-
-		lineStream.on('line', function(line) {
-			lastPromise = lastPromise.then(function(builder) {
-
-				var matches = /^@import (?:\(inline\) )?[\'"](.+?)[\'"];/.exec(line);
-				if (!matches) {
-
-					if (debug) {
-						gutil.log('=> ' + line);
-					}
-
-					return builder.appendLine(line);
-
-				}
-
-				var importpath = matches[1];
-				if (!paths.extname(importpath)) {
-					importpath += '.less';
-				}
-
-				var fileDir = /^[\/\\]/.test(importpath)
-						? process.cwd()
-						: paths.dirname(filepath);
-
-				var targetPath = paths.resolve(fileDir, './' + importpath);
-				return scanFile(targetPath, promise, context, debug);
-
-			});
-		});
-
-		lineStream.on('error', function(error) {
-			gutil.log('Stream error');
-			reject(error);
-		});
-
-		lineStream.on('close', function() {
-			lastPromise.then(resolve).catch(reject);
-		});
-
-	});
-}
-
-/**
- * @param {File} filepath The path to the file being scanned.
- * @param {Boolean} debug Whether to display debug messages during work.
- * @returns {Promise<Buffer>}
- **/
-function startScan(filepath, debug) {
-	gutil.log('Starting style precompilation with ' + filepath);
-	return scanFile(filepath, Promise.resolve(new StringBuilder()), {}, debug);
-}
+var scanner = require('./scanner');
 
 /**
  * Creates a new precompiler for use in a gulp workflow.
@@ -110,7 +27,7 @@ module.exports = function lessPrecompiler(options) {
 			done(null, content);
 		}
 
-		startScan(filepath, config.debug).then(function(builder) {
+		scanner(filepath, config.debug).then(function(builder) {
 			return builder.build(function(error, result) {
 
 				if (error) {
---
+++ src/main/scanner.js
@@ -0,0 +1,101 @@
+/* global require, process, Buffer */
+'use strict';
+
+var gutil = require('gulp-util');
+var files = require('fs');
+var readline = require('readline');
+var paths = require('path');
+var StringBuilder = require('stringbuilder');
+var Promise = require('promise');
+
+/**
+ *
+ * @param {File} filepath The path to the file being scanned.
+ * @param {Promise<Buffer>} promise A promise resolving to the buffer output.
+ * @param {Object} context
+ * @param {Boolean} debug Whether to display debug messages during work.
+ * @param {Array} [counter] Contains the current path counter.
+ * @returns {Promise<Buffer>}
+ */
+function scanFile(filepath, promise, context, debug, counter) {
+	return new Promise(function(resolve, reject) {
+
+		counter && counter.push(0);
+
+		// Make sure the file hasn't already been processed
+		if (context[filepath]) {
+			gutil.log('===> ' + filepath + ' already processed.');
+			counter && counter.pop();
+			return promise.then(resolve, reject);
+		}
+
+		gutil.log('===> Processing ' + filepath);
+
+		// Add target file to the context to avoid repeats.
+		context[filepath] = true;
+
+		// Start the promise chain.
+		var lastPromise = promise;
+
+		var lineStream = readline.createInterface({
+			input: files.createReadStream(filepath),
+			terminal: false
+		});
+
+		lineStream.on('line', function(line) {
+			lastPromise = lastPromise.then(function(builder) {
+				counter && counter[counter.length - 1]++;
+
+				var matches = /^@import (?:\(inline\) )?[\'"](.+?)[\'"];/.exec(line);
+				if (!matches) {
+
+					if (debug) {
+						gutil.log('=> ' + line);
+					}
+
+					return builder.appendLine(line + ( counter
+									? " // " + counter.join(" / ")
+									: "" ));
+
+				}
+
+				var importpath = matches[1];
+				if (!paths.extname(importpath)) {
+					importpath += '.less';
+				}
+
+				var fileDir = /^[\/\\]/.test(importpath)
+						? process.cwd()
+						: paths.dirname(filepath);
+
+				var targetPath = paths.resolve(fileDir, './' + importpath);
+				return scanFile(targetPath, promise, context, debug, counter);
+
+			});
+		});
+
+		lineStream.on('error', function(error) {
+			gutil.log('Stream error');
+			reject(error);
+		});
+
+		lineStream.on('close', function() {
+			lastPromise.then(function(builder) {
+				counter && counter.pop();
+				resolve(builder);
+
+			}).catch(reject);
+		});
+
+	});
+}
+
+/**
+ * @param {File} filepath The path to the file being scanned.
+ * @param {Boolean} debug Whether to display debug messages during work.
+ * @returns {Promise<Buffer>}
+ **/
+module.exports = function startScan(filepath, debug) {
+	gutil.log('Starting style precompilation with ' + filepath);
+	return scanFile(filepath, Promise.resolve(new StringBuilder()), {}, debug, debug ? [] : undefined);
+};
